d4f1445a17e94b0a77ef3df28547e57d7a8d80ed
Rohit Meena <rrrohitmeena@gmail.com>
2026-08-31 23:55:13 +0530

    Add 1901A Line Trip solution

---
+++ 800/1901A_Line_Trip.cpp
@@ -0,0 +1,33 @@
+// Codeforces Problem: 1901A - Line Trip
+// Rating: 800
+// Problem Link: https://codeforces.com/problemset/problem/1901/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+
+        int prev = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+
+            ans = max(ans, a - prev);
+            prev = a;
+        }
+
+        ans = max(ans, 2 * (x - prev));
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
