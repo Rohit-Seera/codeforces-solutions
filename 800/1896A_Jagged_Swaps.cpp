b3750b5377742a3c8a5227809722ab333d10c149
Rohit Meena <rrrohitmeena@gmail.com>
2026-09-01 13:57:45 +0530

    Rename 1896A solution to Jagged Swaps

---
+++ 800/1896A_Jagged_Swaps.cpp
@@ -0,0 +1,39 @@
+// Codeforces Problem: A. Jagged Swaps
+// Rating: 800
+// Problem Link: https://codeforces.com/problemset/problem/1896/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    vector<int> copy_v = v;
+    sort(copy_v.begin(), copy_v.end());
+
+    if (copy_v == v) {
+        cout << "Yes" << endl;
+        return;
+    } else if (v[0] == 1) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
