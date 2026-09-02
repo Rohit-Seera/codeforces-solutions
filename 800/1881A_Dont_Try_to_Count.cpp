2b62c8ba9369f04b5b8fbe7a0c915415a4f70ff4
Rohit Meena <rrrohitmeena@gmail.com>
2026-09-02 23:57:22 +0530

    Add 1881A Don't Try to Count solution

---
+++ 800/1881A_Dont_Try_to_Count.cpp
@@ -0,0 +1,37 @@
+// Codeforces Problem: A. Don't Try to Count
+// Rating: 800
+// Problem Link: https://codeforces.com/problemset/problem/1881/A
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    string x, s;
+    cin >> x >> s;
+
+    for (int i = 0; i <= 6; ++i) {
+        if (x.find(s) != string::npos) {
+            cout << i << "\n";
+            return;
+        }
+        x += x;
+    }
+
+    cout << -1 << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
