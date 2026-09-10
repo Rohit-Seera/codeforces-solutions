cdc82b0495871414e272aa94268ddf2c4e39e025
Rohit Meena <rrrohitmeena@gmail.com>
2026-09-10 22:24:07 +0530

    Add 1950A Stair, Peak, or None solution

---
+++ 800/1950A_Stair_Peak_or_None.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a < b && b < c) {
+        cout << "STAIR\n";
+    } else if (a < b && b > c) {
+        cout << "PEAK\n";
+    } else {
+        cout << "NONE\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
