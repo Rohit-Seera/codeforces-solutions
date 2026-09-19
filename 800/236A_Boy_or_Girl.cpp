1c472312c57d903496136e305791c5c56a1cbcc7
Rohit Meena <rrrohitmeena@gmail.com>
2026-09-19 23:29:42 +0530

    Add 236A solution

---
+++ 800/236A_Boy_or_Girl.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+ 
+int main() { 
+    string s; 
+    cin >> s; 
+ 
+    set<char> uniqueChars; 
+ 
+    for (char c : s) { 
+        uniqueChars.insert(c); 
+    } 
+ 
+    if (uniqueChars.size() % 2 == 0) 
+        cout << "CHAT WITH HER!"; 
+    else 
+        cout << "IGNORE HIM!"; 
+ 
+    return 0; 
+}
